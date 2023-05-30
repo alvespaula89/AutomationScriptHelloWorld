7d70219a9a560f9cdf416312ed17d901086387c0
Paula Rodrigues Ferreira Alves <paula.alves@g.globo>
2023-05-30 16:50:30 -0300

    change2

--- HelloWord_1/HelloWord_1.cs
+++ HelloWord_1/HelloWord_1.cs
@@ -68,7 +68,7 @@ namespace HelloWord_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello WorldDesktop");
 
 			/// change
 		}
