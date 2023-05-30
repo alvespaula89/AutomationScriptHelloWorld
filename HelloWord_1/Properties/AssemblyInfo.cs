0e11e48e4a82a900ed8414e19361d0ab9ffda0fe
Paula <Paula@DESKTOP-T3T1JGV>
2023-05-30 16:21:00 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWord_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWord_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4C6EE7C9-7E4C-4CB8-9AAC-6B29F23F51CC")]
+[assembly: AssemblyVersion("1.0.0.0")]
